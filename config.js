94b2f5e3738e7b35ed11a973787bf1d7e6577202
Sam Griesemer <samgriesemer@gmail.com>
2018-06-26 09:56:17 -0500

    add global config file, remove line.js temporarily

---
+++ config.js
@@ -0,0 +1,26 @@
+// global values across all chart scripts
+var margin = {top: 30, right: 30, bottom: 30, left: 30},
+    width = window.innerWidth - margin.left - margin.right,
+    height = window.innerHeight - margin.top - margin.bottom,
+    full_width = width + margin.left + margin.right,
+    full_height = height + margin.top + margin.bottom;
+    
+// default color scheme
+var scheme = ['#d1e4f1', '#f7f00a', '#EE4266', '#540D6E'],
+    clr = d3.scaleOrdinal(scheme);
+
+var format = d3.format(',d');
+
+// append tooltip div to DOM
+var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("display", "none");
+
+// mouse hover functions
+function mouseover() {
+    div.style("display", "inline");
+}
+
+function mouseout() {
+    div.style("display", "none");
+}
